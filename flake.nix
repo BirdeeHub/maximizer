b7426ea58c2b7a656e273c72790dcda000e79068
BirdeeHub <robindbor@gmail.com>
2024-09-20 00:01:19 -0700

    I just wanna tell you how I'm feeling
    (auto-msg whatthecommit.com)
    
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      (use "git restore --staged <file>..." to unstage)
    	modified:   flake.nix

--- flake.nix
+++ flake.nix
@@ -2,32 +2,21 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
-  outputs = { nixpkgs, ... }@inputs: let
-    forEachSystem = with builtins; f: let # flake-utils.lib.eachSystem
-      op = attrs: system: let
-        ret = f system;
-        op = attrs: key: attrs // {
-          ${key} = (attrs.${key} or { })
-          // { ${system} = ret.${key}; };
-        };
-      in foldl' op attrs (attrNames ret);
-    in foldl' op { } nixpkgs.lib.platforms.all;
-
+  outputs = { nixpkgs, ... }: let
+    forAllSys = nixpkgs.lib.genAttrs nixpkgs.lib.platforms.all;
     APPNAME = "maximize_program";
     appOverlay = final: prev: {
-      # any pkgs overrides made here will be inherited in the arguments of default.nix
-      # because we used final.callPackage instead of prev.callPackage
       ${APPNAME} = final.callPackage ./. { inherit APPNAME; };
     };
   in {
     overlays.default = appOverlay;
-  } // (
-    forEachSystem (system: let
-      pkgs = import nixpkgs { inherit system; overlays = [ appOverlay ]; };
-    in{
-      packages = {
-        default = pkgs.${APPNAME};
+    packages = forAllSys (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ appOverlay ];
       };
-    })
-  );
+    in {
+      default = pkgs.${APPNAME};
+    });
+  };
 }
